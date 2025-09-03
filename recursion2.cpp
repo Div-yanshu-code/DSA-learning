21658bb5c2c709054a3c50e5c20951270ecd2790
Divyanshu sharma <divyanshu09872@gmail.com>
2025-09-03 15:56:34 +0530

    added functional recursion

--- recursion2.cpp
+++ recursion2.cpp
@@ -1,14 +1,50 @@
+// #include<iostream>
+// using namespace std;
+
+// // sum of first n numbers using recursion!!
+// void rSum(int i , int sum){
+
+// if (i< 1){
+//     cout<< sum ;
+// return;
+// }
+// rSum(i-1 , sum+i);
+
+
+
+// }
+
+
+
+
+
+// int main (){
+
+
+
+// int  n ;
+// cout<<"Enter the value of n  upto which you want the sum :-";
+// cin>> n;
+// rSum(n , 0);
+
+
+
+
+
+
+
+
+
+// }
+// sum using functional recursion:-
 #include<iostream>
 using namespace std;
 
-// sum of first n numbers using recursion!!
-void rSum(int i , int sum){
-
-if (i< 1){
-    cout<< sum ;
-return;
+int sum (int n) { 
+if(n==0) return 0 ;
+else{
+cout<<  n + sum (n-1);
 }
-rSum(i-1 , sum+i);
 
 
 
@@ -17,22 +53,12 @@ rSum(i-1 , sum+i);
 
 
 
-
-int main (){
-
+int main() {
 
 
-int  n ;
-cout<<"Enter the value of n  upto which you want the sum :-";
-cin>> n;
-rSum(n , 0);
-
-
-
-
-
-
-
-
-
+    int n;
+    cout<< "Enter the value of n : -";
+    cin>> n;
+    
+    sum (n);
 }
